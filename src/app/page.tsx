606f13cbd0ec58ec32e5c08aec93a2550004a8bb
Z User <z@container>
2026-04-20 17:22:19 +0000

    0f6526ee-5486-4531-bd5a-9bde381c118e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -726,7 +726,7 @@ export default function Home() {
         {/* Background Image */}
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: "url('/images/hero-bg.png')" }}
+          style={{ backgroundImage: "url('/images/hero-bg.png?v=4')" }}
         />
 
         {/* Dark overlay */}
