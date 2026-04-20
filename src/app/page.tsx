c5e9e842ec01d238904b38df5212dd465f7291c2
Z User <z@container>
2026-04-20 17:27:22 +0000

    95481465-e42b-4049-af3c-e4b43ddd724b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -730,7 +730,7 @@ export default function Home() {
         />
 
         {/* Dark overlay */}
-        <div className="absolute inset-0 bg-mystic-950/40" />
+        <div className="absolute inset-0 bg-mystic-950/70" />
 
         {/* Floating stars */}
         {stars.map((star) => (
