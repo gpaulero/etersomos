3e102445341fc31e6090c2a8e9f0082b2a5a8e23
Z User <z@container>
2026-04-20 17:37:58 +0000

    90f658dd-7974-49ef-b112-4e5771bb1367

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -730,7 +730,7 @@ export default function Home() {
         />
 
         {/* Dark overlay */}
-        <div className="absolute inset-0 bg-mystic-950/85" />
+        <div className="absolute inset-0 bg-mystic-950/80" />
 
         {/* Bottom fade to next section */}
         <div className="absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-t from-background to-transparent z-[1]" />
@@ -1185,7 +1185,7 @@ export default function Home() {
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{ backgroundImage: "url('/images/crystals-banner.png')" }}
         />
-        <div className="absolute inset-0 bg-mystic-950/85" />
+        <div className="absolute inset-0 bg-mystic-950/80" />
         {/* Top fade from previous section */}
         <div className="absolute top-0 left-0 right-0 h-32 bg-gradient-to-b from-background to-transparent z-[1]" />
         {/* Bottom fade to next section */}
